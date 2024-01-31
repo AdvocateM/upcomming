395287ff42a1f0d2ebdfb2e229aefc17d36f0c09
AdvocateM <tboymaroga7@gmail.com>
2024-01-31 22:01:30 +0000

    extra time...

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function countdown(){
-    const countDate = new Date(`february 1, 2024 00:00:00`).getTime()
+    const countDate = new Date(`february 5, 2024 00:00:00`).getTime()
     const now = new Date().getTime();
     const gap = countDate - now;
 
